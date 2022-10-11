5f97c0b4c7b8f605ee99d9838c2caaa6cf1b6d02
littlemaiky <016201232j@uandina.edu.pe>
2022-10-10 19:00:03 -0500

    primero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { ImagenesComponent } from './componentes/imagenes/imagenes.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ImagenesComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
